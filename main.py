530219af2cb561e3debb5eace55085189750ebc8
Sungho Bae <45239582+oy6uns@users.noreply.github.com>
2024-06-04 02:39:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -155,4 +155,4 @@ async def makeIcon_URL(query: str):
     return {"answer": answer, "references": references}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port = 8000)
+    uvicorn.run(app, host="0.0.0.0", port = 8000)
